f1597cbfef16eaf1ab2c38403765b3cbb4640a79
Andres Rodriguez <anrocox@gmail.com>
2014-07-21 15:09:29 -0500

    In Python, How to approximate a float to the nearest integer?

---
+++ src/round_float.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 16, 2014
+
+@author: anroco
+
+In Python, How to approximate a float to the nearest integer?
+
+En Python, ¿Cómo aproximar un float a entero más cercano?
+'''
+
+#create a float number
+f = 13.74
+print(f)
+
+#the value is rounded to the closest multiple of 10 to the power.
+print(round(f))
+
+#create a negative number
+f = -45.29
+print(f)
+print(round(f))
